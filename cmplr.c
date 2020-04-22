ac4e940ccef5d3fa84ada5013572a69df3576c42
sumi yuuki <sumiyu0621@gmail.com>
2020-04-22 17:33:27 +0900

    LL(1)パーサの作成

--- cmplr.c
+++ cmplr.c
@@ -22,11 +22,33 @@ struct Token {
     char *str;       // トークン文字列
 };
 
+// 抽象構文木のノードの種類
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node {
+    NodeKind kind;  // ノードの型
+    Node *lhs;      // 左辺
+    Node *rhs;      // 右辺
+    int val;        // kindがND_NUMの場合のみ使う
+};
+
 // 現在着目しているトークン
 Token *token;
 
 // 入力プログラム
 char *user_input;
+
+// 関数の宣言
+Node *expr();
 
 // エラー箇所を特定する
 void error_at(char *loc, char *fmt, ...) {
@@ -84,6 +106,65 @@ int expect_number() {
 
 bool at_eof() {
     return token->kind == TK_EOF;
+}
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *primary() {
+    // 次のトークンが"("なら"(" expr ")" のはず
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    // そうでなければ数値のはず
+    return new_node_num(expect_number());
+}
+
+Node *mul() {
+    Node *node = primary();
+
+    for (;;) {
+        if (consume('*')) {
+            node = new_node(ND_MUL, node, primary());
+        }
+        else if (consume('/')) {
+            node = new_node(ND_DIV, node, primary());
+        }
+        else {
+            return node;
+        }
+    }
+}
+
+Node *expr() {
+    Node *node = mul();
+
+    for (;;) {
+        if (consume('+')) {
+            node = new_node(ND_ADD, node, mul());
+        }
+        else if (consume('-')) {
+            node = new_node(ND_SUB, node, mul());
+        }
+        else {
+            return node;
+        }
+    }
 }
 
 // 新しいトークンを作成してcurに繋げる
