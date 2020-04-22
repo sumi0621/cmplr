5582127ae361e0455bcea92ece164f3fccea04e8
sumi yuuki <sumiyu0621@gmail.com>
2020-04-22 17:04:53 +0900

    エラーメッセージを改良

--- cmplr.c
+++ cmplr.c
@@ -25,6 +25,23 @@ struct Token {
 // 現在着目しているトークン
 Token *token;
 
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を特定する
+void error_at(char *loc, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, ""); // pos個の空白を出力
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
 // エラーを報告するための関数
 // printfと同じ引数を取る
 void error(char *fmt, ...) {
@@ -49,7 +66,7 @@ bool consume(char op) {
 // それ以外の場合にはエラーを報告する
 void expect(char op) {
     if (token->kind != TK_RESERVED || token->str[0] != op) {
-        error("'%c'ではありません", op);
+        error_at(token->str, "'%c'ではありません", op);
     }
     token = token->next;
 }
@@ -58,7 +75,7 @@ void expect(char op) {
 // それ以外の場合にはエラーを報告する
 int expect_number() {
     if (token->kind != TK_NUM) {
-        error("数ではありません");
+        error_at(token->str, "数ではありません");
     }
     int val = token->val;
     token = token->next;
@@ -102,7 +119,7 @@ Token *tokenize(char *p) {
             continue;
         }
 
-        error("トークナイズできません");
+        error_at(cur->str, "トークナイズできません");
     }
 
     new_token(TK_EOF, cur, p);
@@ -115,8 +132,10 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    user_input = argv[1];
+
     // トークナイズする
-    token = tokenize(argv[1]);
+    token = tokenize(user_input);
 
     // アセンブリの前半部分を出力
     printf(".intel_syntax noprefix\n");
