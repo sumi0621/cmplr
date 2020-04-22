74a856b412bcd11ef49f4f9efad5e8bc2b0e0344
sumi yuuki <sumiyu0621@gmail.com>
2020-04-22 18:16:52 +0900

    四則演算機能の追加

--- cmplr.c
+++ cmplr.c
@@ -189,7 +189,7 @@ Token *tokenize(char *p) {
             continue;
         }
 
-        if (*p == '+' || *p == '-') {
+        if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
             cur = new_token(TK_RESERVED, cur, p++);
             continue;
         }
@@ -213,17 +213,17 @@ void gen(Node *node) {
         return;
     }
 
-    get(node->lhs);
+    gen(node->lhs);
     gen(node->rhs);
 
     printf("  pop rdi\n");
     printf("  pop rax\n");
 
-    wsitch (node->kind) {
+    switch (node->kind) {
         case ND_ADD:
             printf("  add rax, rdi\n");
             break;
-        case ND_SUM:
+        case ND_SUB:
             printf("  sub rax, rdi\n");
             break;
         case ND_MUL:
@@ -244,30 +244,21 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    // トークナイズしてパースする
     user_input = argv[1];
-
-    // トークナイズする
     token = tokenize(user_input);
+    Node *node = expr();
 
     // アセンブリの前半部分を出力
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    // 式の最初は数でなければならないので、それをチェックして
-    // 最初のmov命令を出力
-    printf("  mov rax, %d\n", expect_number());
+    // 抽象構文木を下りながらコード生成
+    gen(node);
 
-    // '+ <数>' あるいは '- <数>'というトークンの並びをしょうひしつつアセンブリを出力
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("  add rax, %d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf("  sub rax, %d\n", expect_number());
-    }
+    // スタックトップに式全体の値が残っているはずなのでそれをRAXにロードして関数からの返り値とする
+    printf("  pop rax\n");
     printf("  ret\n");
     return 0;
 }
