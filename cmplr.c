497fc52cce285bc4ca81c0e9358fbe642ed574b2
sumi yuuki <sumiyu0621@gmail.com>
2020-04-22 18:11:00 +0900

    レジスタマシンでスタックマシンを実装

--- cmplr.c
+++ cmplr.c
@@ -207,6 +207,37 @@ Token *tokenize(char *p) {
     return head.next;
 }
 
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    get(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    wsitch (node->kind) {
+        case ND_ADD:
+            printf("  add rax, rdi\n");
+            break;
+        case ND_SUM:
+            printf("  sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf(" imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            printf("  cqo\n");
+            printf("  idiv rdi\n");
+            break;
+    }
+
+    printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "引数の個数が正しくありません\n");
